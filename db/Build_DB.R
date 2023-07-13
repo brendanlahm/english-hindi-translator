6b9b729ad7ea571c2030df0c0ac1ea6b05c51079
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2023-07-13 22:10:49 +0200

    Removed extra spaces & duplicate entries in the DB

--- db/Build_DB.R
+++ db/Build_DB.R
@@ -11,6 +11,12 @@ dict$egrammar[65232] <- "Adjective"
 dict$egrammar <- gsub('Adjective\"', replacement='Adjective', x=dict$egrammar)
 substr(dict$egrammar, 1, 1) <- str_to_upper(substr(dict$egrammar, 1, 1))
 dict$eword <- str_replace_all(dict$eword, "^[:space:]", "")
+dict$hword <- str_replace_all(dict$hword, "^[:space:]", "")
+dict$eword <- str_replace_all(dict$eword, "[:space:]{2}", " ")
+dict$hword <- str_replace_all(dict$hword, "[:space:]{2}", " ")
+
+############################# Remove duplicate rows
+dict <- unique(dict[,1:3])
 
 
 ########## Establish connection & write
@@ -18,6 +24,11 @@ hindi_db <- dbConnect(RSQLite::SQLite(), "../db/hindi_english.db")
 
 dbWriteTable(hindi_db, "dict", dict, overwrite=F)
 
+########## For overwriting
+dbDisconnect(hindi_db)
+hindi_db <- dbConnect(RSQLite::SQLite(), "../db/hindi_english.db")
+
+dbWriteTable(hindi_db, "dict", dict, overwrite=T)
 
 
 
